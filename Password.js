01686660f43138aca4cb503afb8107f25493d064
Divyansh Singh <singhdivyansh0890@gmail.com>
2026-01-09 02:37:12 +0530

    Add files via upload

---
+++ Password.js
@@ -0,0 +1,31 @@
+
+const PasswordBox = document.getElementById("Password");
+const passwordLength = 12;
+
+
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const symbolChars = "!@#$%^&*()_+-=";
+
+const allowedChars = lowercaseChars + uppercaseChars + numberChars + symbolChars;
+
+function generatePassword(){
+    
+    let password = "";
+
+    password += uppercaseChars[Math.floor(Math.random() * uppercaseChars.length)]
+    password += lowercaseChars[Math.floor(Math.random() * lowercaseChars.length)]
+    password += numberChars[Math.floor(Math.random() * numberChars.length)]
+    password += symbolChars[Math.floor(Math.random() * symbolChars.length)]
+    
+    while(passwordLength > password.length){
+        password += allowedChars[Math.floor(Math.random() * allowedChars.length)];
+    }
+    PasswordBox.value = password;
+}
+
+function copyPassword(){
+    PasswordBox.select();
+    document.execCommand("copy");
+}
